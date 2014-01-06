9cbe13499c6f8e20f16bf23375c6393dc489bffc
shanshan <sszhupku@gmail.com>
2014-01-05 20:55:25 -0800

    add spinner

--- client/scripts/view/urlInputView.js
+++ client/scripts/view/urlInputView.js
@@ -2,16 +2,21 @@ define(["backbone","jquery","handlebar"], function(){
 
   var UrlInputView = Backbone.View.extend({
 //add filter for linkedin
-    template:"<form method='post' action='/submiturl.html'> \
+    template: "<span> <form method='post' action='/submiturl.html'> \
       <input class='urlinput' type='text' name='urlinput' placeholder='http://www.linkedin.com/' autofocus> \
       <input type='submit' value='Go'> \
-      </form>",
+      </form></span> \
+      <span class='spinner'>",
+
+    templateSpinner: "<img src='img/spinner.gif' width='30' />",
     
     events: {
       "submit": "checkUrl"
     },
     checkUrl: function(e){
       e.preventDefault();
+      e.target.children[1].setAttribute("disabled","disabled");
+      this.$('.spinner').html(this.templateSpinner);
       var self = this; //to be used in $.post;
       var text = $('.urlinput').val();
       if (text.indexOf("http://") === -1 || text.indexOf("linkedin.com/") === -1 ) {
@@ -22,6 +27,12 @@ define(["backbone","jquery","handlebar"], function(){
         self.collection.reset(data);
         self.$el.html(''); //remove the inputBox View;
         console.log(data);
+      })
+      .done(function(){
+
+      })
+      .fail(function(){
+        e.target.children[1].removeAttribute("disabled");
       })
     },
     render: function() {
