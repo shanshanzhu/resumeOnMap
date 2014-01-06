9eeee4ef24f18abc41cfe2eb4e234a497f1709fb
shanshan <sszhupku@gmail.com>
2014-01-05 20:56:18 -0800

    use add to trigger single textTag view

--- client/scripts/view/textTagsView.js
+++ client/scripts/view/textTagsView.js
@@ -1,36 +1,49 @@
 define(["./textTagView", "backbone","jquery","handlebar"], function(TextTagView){
 
-  var textTagsView = Backbone.Collection.extend({
+  var textTagsView = Backbone.View.extend({
 
     initialize: function(options) {
-      _.extend(this.options);
-      this.collection.on('add', this.render, this);//could be updated in future.
+      _.extend(this, options);
+
+      this.datum = this.model.get('content');
+      this.textTags = this.model.get('textTags');
+      this.textTags.on('add', this.renderNext, this);//could be updated in future.
     },
 
-    render: function(textTag) {
-      //to do: setTimeout parameters
+    render: function() {
+      console.log(this.model);
       //to do : add connection lines;
+      if (this.textTags.length === 0) {
+        this.textTags.add([this.datum[0]]);
+      }
+    },
+
+    renderNext: function(textTagModel) {
       debugger;
-      var self = this;
-      this.collection.each(function(textTag){
-        var currentView = new TextTagView({
-            model: textTag,
-            map: self.map
-        });
-        currentView.on
-      });
-      var models = this.collectionModel.models;
-      var init = function(i, currentView) {
-        setTimeout(function() {
-          debugger;
-          if (currentView) { currentView.destroyView();}
-          if (i < models.length -1) {
-            init(i+1, currentView);
-          }
-        },3000);
-      };
-      init(0);
+      console.log(textTagModel);
     }
+
+    //   var currentMarker = new TextTagView({
+    //       model: textTag,
+    //       map: self.map
+    //   });
+
+    //   var self = this;
+    //   this.collection.each(function(textTag){
+    //     currentView.on
+    //   });
+    //   var models = this.collectionModel.models;
+    //   var init = function(i, currentView) {
+    //     setTimeout(function() {
+    //       debugger;
+    //       if (currentView) { currentView.destroyView();}
+    //       if (i < models.length -1) {
+    //         init(i+1, currentView);
+    //       }
+    //     },3000);
+    //   };
+    //   init(0);
+    // }
 
   });
 
