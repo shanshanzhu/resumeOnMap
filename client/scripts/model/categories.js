09e0b185ee6d4dc44fb9198d9bf13de6ea2e4c7f
shanshan <sszhupku@gmail.com>
2014-01-11 13:01:10 -0800

    move the model inside

--- client/scripts/model/categories.js
+++ client/scripts/model/categories.js
@@ -3,10 +3,20 @@ define(["./textTags", "backbone","jquery","handlebar"], function(textTags){
   var category = Backbone.Model.extend({
     
     initialize: function() {
-        this.set("textTags", new textTags());
-    }
+      this.set("textTags", new textTags());
+      this.datum = this.get('content');
+      this.textTags = this.get('textTags');
+    },
     //initialize an empty collection to allow for async add event
 
+    addTag: function() {
+      console.log('addTag in category model');
+      if (this.datum.length > 0) {
+        this.textTags.add([this.datum.shift()]);
+        //the argument of .add takes an array!!
+      }
+
+    }
   });//should be put ahead so that category is defined.
 
   var categories = Backbone.Collection.extend({
