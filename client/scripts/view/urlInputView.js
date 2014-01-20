7d5a3ce4ce64745af789e3afeef5d57d31ceaf9a
shanshan <sszhupku@gmail.com>
2014-01-19 16:12:38 -0800

    fix bug so the query is not started with wrong format of url input

--- client/scripts/view/urlInputView.js
+++ client/scripts/view/urlInputView.js
@@ -16,12 +16,13 @@ define(["backbone","jquery","handlebar"], function(){
     checkUrl: function(e){
       e.preventDefault();
       e.target.children[1].setAttribute("disabled","disabled");
-      this.$('.spinner').html(this.templateSpinner);
       var self = this; //to be used in $.post;
       var text = $('.urlinput').val();
       if (text.indexOf("http://") === -1 || text.indexOf("linkedin.com/") === -1 ) {
         alert('please enter a valid linkedin public url');
+        return;
       }
+      this.$('.spinner').html(this.templateSpinner);
       $.post('/submiturl',{urlinput: text}, function(data){
         // window.location.hash = 'submiturl';//to redirect to submiturl
         self.collection.reset(data);
