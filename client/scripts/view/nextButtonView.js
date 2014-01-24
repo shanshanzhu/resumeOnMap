755a57bf4cf31fc1f5697990e39e19192ac7723a
shanshan <sszhupku@gmail.com>
2014-01-24 13:59:04 -0800

    detect the inactive pre next but-add prev button

--- client/scripts/view/nextButtonView.js
+++ client/scripts/view/nextButtonView.js
@@ -2,19 +2,47 @@ define(["backbone","jquery","handlebar"], function(){
 
 
   var Controller = Backbone.View.extend({
-    template:'<button class="nextButtonInner">NEXT</button>',
+    template:'<button class="prevButton">Previous</button> \
+    <button class="nextButtonInner">NEXT</button>',
+    
+    initialize: function(){
+      this.registerInactivate('inactivate-prevButton');
+      this.registerInactivate('inactivate-nextButtonInner');
+      this.registerActivate('activate-prevButton');
+      this.registerActivate('activate-nextButtonInner');
+    },
 
     events: {
-      'click': "renderNext"
+      'click .prevButton': "renderPrevious",
+      'click .nextButtonInner': "renderNext"
+    },
+
+    registerActivate: function(eventName) {
+      var className = '.'+eventName.split('-')[1];
+      this.collection.on(eventName, function() {
+        if (this.$(className).hasClass('inactive')) {
+          this.$(className).removeClass('inactive');
+        }
+      }, this);
+    },
+
+    registerInactivate: function(eventName) {
+      var className = '.'+eventName.split('-')[1];
+      this.collection.on(eventName, function() {
+        this.$(className).addClass('inactive');
+      }, this);
     },
 
     renderNext: function(e) {
-      this.model.renderNext();
+      this.currentModel.renderNext();
+    },
+
+    renderPrevious: function() {
+      this.currentModel.renderPrevious();
     },
 
     render: function () {
       this.$el.html(this.template);
-      //to fix. position
       return this;
     }
 
